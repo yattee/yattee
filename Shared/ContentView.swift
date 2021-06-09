d488ee6679b46ab9deedc5651adffa8d8044b0ec
Arkadiusz Fal <arek@arekf.net>
2021-06-09 22:51:23 +0200

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Arkadiusz Fal on 09/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/PearvidiousApp.swift
@@ -0,0 +1,17 @@
+//
+//  PearvidiousApp.swift
+//  Shared
+//
+//  Created by Arkadiusz Fal on 09/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct PearvidiousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
