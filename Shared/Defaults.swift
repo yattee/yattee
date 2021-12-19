9b2209c9b5e3a3336e9978fb94448782cec92a84
Arkadiusz Fal <arek@arekf.net>
2021-12-19 18:18:01 +0100

    Update default list of favorites

--- Shared/Defaults.swift
+++ Shared/Defaults.swift
@@ -22,7 +22,12 @@ extension Defaults.Keys {
 
     static let favorites = Key<[FavoriteItem]>("favorites", default: [
         .init(section: .trending("US", "default")),
+        .init(section: .trending("GB", "default")),
+        .init(section: .trending("ES", "default")),
         .init(section: .channel("UC-lHJZR3Gqxm24_Vd_AJ5Yw", "PewDiePie")),
+        .init(section: .channel("UCXuqSBlHAE6Xw-yeJA0Tunw", "Linus Tech Tips")),
+        .init(section: .channel("UCBJycsmduvYEL83R_U4JriQ", "Marques Brownlee")),
+        .init(section: .channel("UCE_M8A5yxnLfW0KghEeajjw", "Apple")),
         .init(section: .searchQuery("Apple Pie Recipes", "", "", ""))
     ])
 
