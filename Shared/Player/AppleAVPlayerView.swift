a9b057505c6a5367c3421d0e4b4ebc0b436f360f
Arkadiusz Fal <arek@arekf.net>
2022-02-27 21:31:17 +0100

    Fixes for MPV in macOS

---
+++ Shared/Player/AppleAVPlayerView.swift
@@ -0,0 +1,25 @@
+import Defaults
+import SwiftUI
+
+struct AppleAVPlayerView: UIViewControllerRepresentable {
+    @EnvironmentObject<CommentsModel> private var comments
+    @EnvironmentObject<NavigationModel> private var navigation
+    @EnvironmentObject<PlayerModel> private var player
+    @EnvironmentObject<SubscriptionsModel> private var subscriptions
+
+    func makeUIViewController(context _: Context) -> UIViewController {
+        let controller = AppleAVPlayerViewController()
+
+        controller.commentsModel = comments
+        controller.navigationModel = navigation
+        controller.playerModel = player
+        controller.subscriptionsModel = subscriptions
+        player.avPlayerBackend.controller = controller
+
+        return controller
+    }
+
+    func updateUIViewController(_: UIViewController, context _: Context) {
+        player.rebuildTVMenu()
+    }
+}
