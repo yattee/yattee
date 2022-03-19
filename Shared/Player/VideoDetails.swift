41939726698174efdf3849aed107396ad6b88d44
Arkadiusz Fal <arek@arekf.net>
2022-03-20 00:05:09 +0100

    Minor fixes

--- Shared/Player/VideoDetails.swift
+++ Shared/Player/VideoDetails.swift
@@ -98,12 +98,8 @@ struct VideoDetails: View {
 
             switch currentPage {
             case .info:
-                if player.isLoadingVideo {
-                    PlaceholderProgressView()
-                } else {
-                    ScrollView(.vertical, showsIndicators: false) {
-                        detailsPage
-                    }
+                ScrollView(.vertical, showsIndicators: false) {
+                    detailsPage
                 }
             case .queue:
                 PlayerQueueView(sidebarQueue: $sidebarQueue, fullScreen: $fullScreen)
