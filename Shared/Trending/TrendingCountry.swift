07ad4b1e5480b6abe9fec36822319f4224179c6c
Arkadiusz Fal <arek@arekf.net>
2022-05-20 21:53:17 +0200

    Lint

--- Shared/Trending/TrendingCountry.swift
+++ Shared/Trending/TrendingCountry.swift
@@ -17,9 +17,9 @@ struct TrendingCountry: View {
             #if !os(tvOS)
                 HStack {
                     if #available(iOS 15.0, macOS 12.0, *) {
-                        TextField("Country", text: $query, prompt: Text(TrendingCountry.prompt))
+                        TextField("Country", text: $query, prompt: Text(Self.prompt))
                     } else {
-                        TextField(TrendingCountry.prompt, text: $query)
+                        TextField(Self.prompt, text: $query)
                     }
 
                     Button("Done") { selectCountryAndDismiss() }
