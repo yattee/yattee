93c9298c0773f4eabf2c868302c7fcd5bee8ec84
Arkadiusz Fal <arek@arekf.net>
2022-12-13 13:17:23 +0100

    Fix badge backport for tvOS

--- Backports/Badge+Backport.swift
+++ Backports/Badge+Backport.swift
@@ -2,10 +2,14 @@ import SwiftUI
 
 extension Backport where Content: View {
     @ViewBuilder func badge(_ count: Text?) -> some View {
-        if #available(iOS 15.0, macOS 12.0, tvOS 15.0, *) {
-            content.badge(count)
-        } else {
+        #if os(tvOS)
             content
-        }
+        #else
+            if #available(iOS 15.0, macOS 12.0, tvOS 15.0, *) {
+                content.badge(count)
+            } else {
+                content
+            }
+        #endif
     }
 }
