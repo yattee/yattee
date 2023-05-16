c78a0dc8c3ff7c0230069b236ce31b15f97cb1d8
Arkadiusz Fal <arek@arekf.net>
2023-05-16 18:51:34 +0200

    Fix playback settings sheet height

--- Shared/Player/PlaybackSettingsPresentationDetents+Backport.swift
+++ Shared/Player/PlaybackSettingsPresentationDetents+Backport.swift
@@ -5,7 +5,7 @@ extension Backport where Content: View {
     @ViewBuilder func playbackSettingsPresentationDetents() -> some View {
         if #available(iOS 16.0, macOS 13.0, tvOS 16.0, *) {
             content
-                .presentationDetents([.height(350), .large])
+                .presentationDetents([.height(400), .large])
         } else {
             content
         }
