b8cde410c53eee1494f35ce2f12d78b98b180aac
Arkadiusz Fal <arek@arekf.net>
2025-11-14 18:58:28 +0100

    Update default visible sections from trending to popular
    Changed the default visible sections to include popular instead of trending.

--- Shared/Defaults.swift
+++ Shared/Defaults.swift
@@ -16,7 +16,7 @@ extension Defaults.Keys {
     static let widgetsSettings = Key<[WidgetSettings]>("widgetsSettings", default: [])
     static let startupSection = Key<StartupSection>("startupSection", default: .home)
     static let showSearchSuggestions = Key<Bool>("showSearchSuggestions", default: true)
-    static let visibleSections = Key<Set<VisibleSection>>("visibleSections", default: [.subscriptions, .trending, .playlists])
+    static let visibleSections = Key<Set<VisibleSection>>("visibleSections", default: [.subscriptions, .popular, .playlists])
 
     static let showOpenActionsToolbarItem = Key<Bool>("showOpenActionsToolbarItem", default: false)
     #if os(iOS)
