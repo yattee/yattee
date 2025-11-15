adf282d0e21a5a594d5a3ff321d640e8037bbbcf
Arkadiusz Fal <arek@arekf.net>
2025-11-15 11:51:00 +0100

    Add left padding to video details overlay on iPad in non-fullscreen mode
    When displaying the video details overlay on iPad in non-fullscreen windows,
    add 65px of left padding (50px for system controls width + 15px spacing) to
    prevent content from overlapping with iPad system controls.

--- Shared/Player/Controls/VideoDetailsOverlay.swift
+++ Shared/Player/Controls/VideoDetailsOverlay.swift
@@ -7,6 +7,7 @@ struct VideoDetailsOverlay: View {
     var body: some View {
         VideoDetails(video: controls.player.videoForDisplay, fullScreen: fullScreenBinding, sidebarQueue: .constant(false))
             .clipShape(RoundedRectangle(cornerRadius: 4))
+            .padding(.leading, overlayLeadingPadding)
             .id(controls.player.currentVideo?.cacheKey)
     }
 
@@ -17,6 +18,20 @@ struct VideoDetailsOverlay: View {
             controls.presentingDetailsOverlay = newValue
         })
     }
+
+    #if os(iOS)
+        private var overlayLeadingPadding: CGFloat {
+            // On iPad in non-fullscreen mode, add left padding for system controls
+            if Constants.isIPad && !Constants.isWindowFullscreen {
+                return Constants.iPadSystemControlsWidth + 15
+            }
+            return 0
+        }
+    #else
+        private var overlayLeadingPadding: CGFloat {
+            return 0
+        }
+    #endif
 }
 
 struct VideoDetailsOverlay_Previews: PreviewProvider {
